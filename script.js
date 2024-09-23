e4ba98f328f87554eb9cff090bde912565d0033f
Quratulain Arif <154021980+ainycodes@users.noreply.github.com>
2024-09-23 11:33:56 +0500

    Create script.js

--- script.js
+++ script.js
@@ -98,3 +98,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registered: ', registration);
+      }).catch(registrationError => {
+        console.log('ServiceWorker registration failed: ', registrationError);
+      });
+  });
+}
